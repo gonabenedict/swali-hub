335f9560d8aa547e63a4988d10c1c313ca822324
gonabenedict <gona.benedict24@students.dkut.ac.ke>
2025-12-14 11:07:26 +0300

    modified backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import userRouter from './routes/userRoutes.js';
 import resultRouter from './routes/resultsRoutes.js';
 
 const app = express();
-const PORT =  4000;
+const PORT = process.env.PORT || 4000;
 
 //Middleware
 app.use(cors());
